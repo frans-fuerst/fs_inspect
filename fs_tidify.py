db4f35ab7b1183d8e6307c9c5cf43d5af0861a6a
frans <frans@scitics.de>
2015-08-23 16:42:26 +0200

    cleanup/reorganize/refactor

--- fs_tidify.py
+++ fs_tidify.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-''' this is the fs_tidify core module. import and operate on FsDb
+''' this is the fs_tidify core module. import and operate on fs_db
 '''
 
 import os
@@ -11,16 +11,24 @@ from functools import partial
 import json
 import pprint
 
+
 def sha1_chunked(filename, chunksize=2**15, bufsize=-1):
     """add docstring"""
-    # http://stackoverflow.com/questions/4949162/max-limit-of-bytes-in-method-update-of-hashlib-python-module
+    # max-limit-of-bytes-in-method-update-of-hashlib-python-module
+    # http://stackoverflow.com/questions/4949162
     sha1_hash = hashlib.sha1()
     with open(filename, 'rb', bufsize) as _file:
         for chunk in iter(partial(_file.read, chunksize), b''):
             sha1_hash.update(chunk)
     return sha1_hash
 
-class FileInfo:
+
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    return type('Enum', (), enums)
+
+
+class file_info:
     """add docstring"""
     def __init__(self, name, path):
         self.name = name
@@ -28,7 +36,7 @@ class FileInfo:
 
     def __str__(self):
         return os.path.join(self.path, self.name)
-    
+
     def __repr__(self):
         return str(self)
 
@@ -46,13 +54,26 @@ class FileInfo:
             raise
         return _hash1
 
-class FileY:
-    """add docstring"""
 
-    def __init__(self, file_info, initial_state=0):
-        self.state = initial_state
-        self.file_info = file_info
-        self.files = None
+class file_y:
+    ''' a file_y object contains one or more descriptions referring to a list
+        of files with identical content.
+        One file_y object has mulitiple levels of identification which make
+        the file distinguishable from each other. E.g. one file_y object can
+        hold several files with different sizes:
+        root |
+             + - 3247 bytes -> [PATH1, PATH2, ...]
+             |
+             + - 5432 bytes -> [PATH3, PATH4, ...]
+    '''
+
+    # level of differentiation
+    states = enum('SIZE', 'HASH', 'IDENTICAL')
+
+    def __init__(self, file_info, initial_state=states.SIZE):
+        self._state = initial_state
+        self._file = file_info  # used with state == SIZE
+        self._hashes = None     # used with state == HASH
 
     @classmethod
     def from_dict(cls, input_dict):
@@ -60,50 +81,74 @@ class FileY:
         new_object.__dict__.update(input_dict)
         return new_object
 
+    def to_JSON(self):
+        return None
+
     def add(self, new_file_info):
-        """add docstring"""
-        if self.state == 0:
-            # if this is the first collision then first promote this FileY
-            # to a 'folder' for equally sized files
-            self.state = 1
-            self.files = {self.file_info.get_hash(): FileY(self.file_info, 2)}
-            self.file_info = None
+        ''' accept new file to register and reorganize tree
+        '''
+        if self._state is file_y.states.SIZE:
+            ''' if this is the first collision then first promote this file_y
+                to a subtree for equally sized files and then recursively add
+                the file again.
+            '''
+            self._state = file_y.states.HASH
+            self._hashes = {self._file.get_hash(): [self._file]}
+            self._file = None
             return self.add(new_file_info)
 
-        elif self.state == 1:
+        elif self._state is file_y.states.HASH:
             _new_hash = new_file_info.get_hash()
-            if not _new_hash in self.files:
-                self.files[_new_hash] = FileY(new_file_info, 2)
-                return 2
+            if _new_hash not in self._hashes:
+                self._hashes[_new_hash] = [new_file_info]
+                return file_y.states.HASH
             else:
-                self.files[_new_hash].add(new_file_info)
-                return 2
+                self._hashes[_new_hash].append(new_file_info)
+                return file_y.states.IDENTICAL
 
-        elif self.state == 2:
-            self.state = 3
-            self.files = [self.file_info, new_file_info]
-            self.file_info = None
-            return 3
-
-        elif self.state == 3:
-            self.files.append(new_file_info)
-            return 3
 
     def get_similar_files(self):
-        if self.state == 0:
-            return []
-        if self.state == 1:
-            return [self.files[x].get_similar_files() for x in self.files
-                    if self.files[x].get_similar_files() != []]
-        if self.state == 2:
-            return []
-        if self.state == 3:
-            return self.files
+        if self._state is file_y.states.SIZE:
+            return ()
+        if self._state is file_y.states.HASH:
+            return tuple(f for h, f in self._hashes.items()
+                    if len(f) > 1)
 
-class FsDb:
-    """a FsDb instance holds all information about files and directories
-       which have been imported and can answer questions about them
-    """
+
+class json_encoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, file_info):
+            return obj.__dict__
+        if isinstance(obj, file_y):
+            return {'state': 'STATE', 'file': obj._file, 'hashes': obj._hashes}
+
+        return json.JSONEncoder.default(self, obj)
+
+
+class json_decoder(json.JSONDecoder):
+
+    def __init__(self, *args, **kargs):
+        json.JSONDecoder.__init__(self, object_hook=self.dict_to_object,
+                             *args, **kargs)
+
+    def dict_to_object(self, d):
+        if '__type__' not in d:
+            return d
+
+        type = d.pop('__type__')
+        try:
+            dateobj = datetime(**d)
+            return dateobj
+        except:
+            d['__type__'] = type
+            return d
+
+
+
+class fs_db:
+    ''' a fs_db instance holds all information about files and directories
+        which have been imported and can answer questions about them
+    '''
 
     def __init__(self, import_export_file = None):
         self._ie_file = import_export_file
@@ -120,16 +165,17 @@ class FsDb:
         return json.dumps(
             { 'directories': self._directories,
               'files': self._files},
-            default=lambda o: o.__dict__,
+            cls=json_encoder,
+            # default=lambda o: o.__dict__,
             sort_keys=True,
             indent=4)
 
     def from_JSON(self, json_data):
-        imported_data = json.loads(json_data)
+        imported_data = json.loads(json_data, cls=json_decoder)
         self._directories = imported_data['directories']
 
         for size, file_y in imported_data['files'].iteritems():
-            self._files[size] = FileY.from_dict(file_y)
+            self._files[size] = file_y.from_dict(file_y)
 
     def print_statistics(self):
         for f in self._files:
@@ -139,29 +185,29 @@ class FsDb:
             pprint.pprint(sim_f)
 
     def register(self, path):
-        """takes a path to investigate and traverses it to
-           index all contained files
-           """
+        ''' takes a path to investigate and traverses it to
+            index all contained files
+        '''
         _totalsize = 0
         _path = os.path.abspath(path)
         logging.debug(_path)
-        for (path, _, files) in os.walk(_path):
+        for (_dir, _, files) in os.walk(_path):
             for fname in files:
-                _fullname = os.path.join(path, fname)
+                _fullname = os.path.join(_dir, fname)
                 if os.path.islink(_fullname):
                     logging.debug("skipping link %s" % _fullname)
                     continue
 
-                _file_info = FileInfo(fname, path)
+                _file_info = file_info(fname, _dir)
 
                 _filesize = os.path.getsize(_fullname)
 
-                logging.debug( "handle: %s", _fullname )
+                logging.debug( "handle: %s (%d)", _fullname, _filesize)
 
                 if not _filesize in self._files:
                     # simple case: file size does not exist yet, so just
                     #              fill the size => file container
-                    self._files[_filesize] = FileY(_file_info)
+                    self._files[_filesize] = file_y(_file_info)
                 else:
                     # collision: size is taken, so we have to treat this
                     #            entry as a 'folder' for equally sized files
@@ -177,18 +223,18 @@ class FsDb:
         return _totalsize
 
     def export_to_fs(self):
-        with open('fst.export.json', 'w') as export_file:
+        with open(self._ie_file, 'w') as export_file:
             export_file.write(self.to_JSON())
 
     def import_from_fs(self):
-        self.from_JSON(open('fst.export.json').read())
+        self.from_JSON(open(self._ie_file).read())
 
 
 def test():
-    ''' make an overall smoke test and explain a typical workflow 
+    ''' make an overall smoke test and explain a typical workflow
     '''
-    
-    fsdb1 = FsDb("fstdb.txt")
+
+    fsdb1 = fs_db("fstdb.txt")
     test_directory = os.path.join(os.path.dirname(__file__), 'example_fs')
     # import information about a directory
     fsdb1.register(test_directory)
@@ -198,7 +244,7 @@ def test():
     fsdb1.export_to_fs()
 
     # load persisted information
-    fsdb2 = FsDb("fstdb.txt")
+    fsdb2 = fs_db("fstdb.txt")
     fsdb2.import_from_fs()
 
     if not fsdb1 == fsdb2:
@@ -209,6 +255,8 @@ def test():
         print('--')
     assert fsdb1 == fsdb2, "equality after loading"
 
+
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     test()
 
