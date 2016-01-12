bb0d6257a4dbe48f52628ba9360b59fac3a3014c
frans <frans@scitics.de>
2016-01-12 21:45:42 +0100

    more of the new program

--- fs_inspect/fsi.py
+++ fs_inspect/fsi.py
@@ -6,70 +6,77 @@ import sys
 import math
 import logging
 
-# expensive - do it only once
-base_dir = os.path.expanduser('~/.fsi/objects')
 
-def get_path(size):
-    p = os.path.join(base_dir, '/'.join('%d' % size))
-    try:
-        os.makedirs(p)
-    except:
-        pass
-    return p
+class indexer:
+    
+    def __init__(self):
+        # expensive - do it only once
+        self._base_dir = os.path.expanduser('~/.fsi/objects')
+
+    def get_path(self, size):
+        _result = os.path.join(
+            self._base_dir, 
+            '/'.join('%d' % size))
+        try:
+            os.makedirs(_result)
+            return (_result, 0)
+        except:
+            if os.path.exists(os.path.join(_result, 'single.txt')):
+                return (_result, 1)
+            else:
+                # todo: assert existing hash files
+                return (_result, 2)
+
+    def add(self, path):
+        _file_count = 0
+    
+        for (_dir, _, files) in os.walk(path):
+            for fname in files:
+                _fullname = os.path.join(_dir, fname)
+                if os.path.islink(_fullname):
+                    logging.debug("skipping link %s" % _fullname)
+                    continue
+                _file_count += 1
+                _filesize = os.path.getsize(_fullname)
+                _time = os.path.getmtime(_fullname)
+                _size_path, _state = self.get_path(_filesize)
+                if _state == 0:
+                    # file size not registered
+                    # create a file with file name and modification date
+                    
+                    pass
+                elif _state == 1:
+                    # single file registered
+                    pass
+                else:
+                    # more than one files already registered
+                    pass
+    
+        print(_file_count)
+
 
 if __name__ == '__main__':
-    _sizes = {}
-    _size_cats = {}
-    _file_count = 0
-
-    _path = sys.argv[1]
-    for (_dir, _, files) in os.walk(_path):
-        for fname in files:
-            _fullname = os.path.join(_dir, fname)
-            if os.path.islink(_fullname):
-                logging.debug("skipping link %s" % _fullname)
-                continue
-            _file_count += 1
-            _filesize = os.path.getsize(_fullname)
-            p = get_path(_filesize)
-            #print(p)
-            _l1 = math.log(_filesize*_filesize,10) if _filesize > 0 else 0
-#            _l2 = math.log(_l1, 10) if _l1 > 0 else 0
-            _size_cat = int(_l1)
-            #print(_filesize, _size_cat)
-            if not _size_cat in _size_cats:
-                _size_cats[_size_cat] = 0
-            _size_cats[_size_cat] += 1
-            if not _filesize in _sizes:
-                _sizes[_filesize] = 0
-            _sizes[_filesize] += 1
-
-            
-
-    # print(_size_cats)
-    print(_file_count)
-    print(len(_sizes))
-    for s in sorted(_size_cats.keys()):
-        print ("%d: %.6d %s" % (s, 
-            _size_cats[s], '*' * int(_size_cats[s]/3000)))
+    p = indexer()
+    p.add(sys.argv[1])
 
 
 '''
 size_cats: 
 
-    0: 002004 
-    1: 014909 ****
-    2: 079085 **************************
-    3: 169468 ********************************************************
-    4: 061196 ********************
-    5: 012829 ****
-    6: 007339 **
-    7: 000890 
-    8: 000047 
-    9: 000016 
+    0:    2004 
+    1:   14909 ****
+    2:   79085 **************************
+    3:  169468 ********************************************************
+    4:   61196 ********************
+    5:   12829 ****
+    6:    7339 **
+    7:     890 
+    8:      47 
+    9:      16 
 
-files: 347783
-sizes: 55014
+files:  347783
+sizes:   55014
+folders: 70877
 
 '''
 
